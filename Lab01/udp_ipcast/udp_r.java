4e2d3b034dd9ce591f44ef8c2f49018c25283e79
isirichard <isi.12ichard@gmail.com>
2019-01-09 20:03:33 -0500

    Archvios de clase

---
+++ Lab01/udp_ipcast/udp_r.java
@@ -0,0 +1,74 @@
+import java.net.*;
+
+// This program waits to receive datagrams sent to a specified port.
+// Este programa espera recibir los datagramas enviados a un puerto específico
+// When it receives one, it displays the sending host and port,
+// Cuando recibe uno, muestra el host de envío y el puerto,
+// and prints the contents of the datagram as a string.
+// e imprime el contenido del datagrama como una cadena.
+
+public class udp_r {
+    public static void main(String args[]) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: java udp_r <port>");
+            System.exit(0);
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        byte[] buffer = new byte[1024];
+        String s;
+
+        // Create a socket to listen on the port.
+        // Crea un socket para escuchar en el puerto.
+        DatagramSocket socket = new DatagramSocket(port);
+        System.out.println("Reception socket created...");
+
+        long expected = 1;
+        
+        for(;;) {
+            // Create a packet with an empty buffer to receive data
+            // Crear un paquete con un búfer vacío para recibir datos
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            // Wait to receive a datagram
+            // Espera recibir un datagrama
+            socket.receive(packet);
+
+
+            // Convert the contents to a string
+            // Convertir el contenido en una cadena
+            s = new String(buffer, 0, packet.getLength());
+
+            // Get the seuqence number as a long
+            // Obtener el número de secuencia como un largo
+            long sequence_number = Long.parseLong(s);
+            // el expected es el número de paquete que calculo y espero 
+            // el número de secuencia es lo que recibo en el datagrama
+            if (sequence_number <= expected) {
+                expected++;
+                System.out.println("udp_r: received from " + 
+                           packet.getAddress().getHostName() + ":" +
+                           packet.getPort() + ": " + s);
+           
+            /*
+            byte[] message = new byte[1024];
+            s2="ping exitoso";
+            message = s.getBytes();
+            DatagramPacket packet2 = new DatagramPacket(message, s.length(), packet.getAddress(), port);
+            socket.send(packet2);
+            */
+            }
+            else {
+                System.out.println("ERROR: unexpected sequence number: " + sequence_number);
+                System.exit(-1);
+            }
+            
+        }
+        
+    }
+}
+
+// Varios emisores a un receptor
+// ¿Que sucedes y por qué? Se crashea error, por el codigo de comparación.
+
---
+++ Lab01/udp_ipcast/udp_s.java
@@ -0,0 +1,51 @@
+import java.net.*;
+
+// This program sends periodically a datagram to the specified (host & port)
+// Este programa envía periódicamente un datagrama al especificado (host y puerto)
+public class udp_s {
+    public static void main(String args[]) throws Exception {
+        if (args.length != 3) {
+            System.out.println("Usage: java udp_s <host> <port> <period in ms>");
+            System.exit(0);
+        }
+
+        // Get the internet address of the specified host and the port number
+        // Obtenga la dirección de Internet del host especificado y el número de puerto
+        InetAddress address = InetAddress.getByName(args[0]);
+        int port = Integer.parseInt(args[1]);
+
+        // Create a socket, and send the packet through it
+        // Crea un socket, y envía el paquete a través de él.
+        DatagramSocket socket = new DatagramSocket();
+        System.out.println("Sending socket created...");
+
+        String s = new String();
+        long sequence_number = 0;
+
+        long period = Long.parseLong(args[2]);
+
+        for (;;) {
+            sequence_number++;
+            Long sequence = new Long(sequence_number);
+            s = sequence.toString();
+
+            // Convert the string s to an array of bytes
+            // Convertir la cadena s a una matriz de bytes
+            byte[] message = new byte[1024];
+            message = s.getBytes();
+
+            // Initialize the packet with data and address
+            // Inicializar el paquete con datos y dirección
+            DatagramPacket packet = new DatagramPacket(message, s.length(), address, port);
+
+            // send the packet through the socket
+            // // enviar el paquete a través del zócalo
+            System.out.println("udp_s: sending message " + sequence_number);
+            socket.send(packet);
+            
+            // Wait for period milliseconds
+            // Espera por periodo milisegundos
+            Thread.sleep(period);
+        }
+    }
+}
